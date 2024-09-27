18382e9ef912dd8b04dd9ebfdc514114f122d724
RuttikaShrirao <ruttikashrirao@gmail.com>
2024-09-27 22:04:43 +0530

    e-commeece store dashboard created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+
+const style = {
+  font_weight: "fw-semibold",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Add Product</h4>
+      <div className={`d-flex p-2 justify-content-evenly `}>
+        <div>
+          <p className={style.font_weight}>Products</p>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Select Product"
+            />
+            <div className="input-group-append">
+              <span className="input-group-text">
+                <i class="bi bi-pencil-fill"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className={style.font_weight}>Discount</p>
+          <Button variant="contained" size="small" color="success">
+            Add Discount
+          </Button>
+        </div>
+      </div>
+      <Button variant="outlined" size="small" color="success">
+        Add Product
+      </Button>
     </div>
   );
 }
